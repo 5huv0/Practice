0b2bb340bcd900697b3c7a1fc942199b5a72fea0
5huv0 <md88748874@gmail.com>
2024-03-15 22:30:22 +0600

    skdjns

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// select dom element 
+// BOM Practice
+
+
+// topic 1
+// window.lenght/height
+// window.open/close
 
 const width = document.getElementById('width');
 const height = document.getElementById('height');
@@ -8,7 +13,7 @@ const height = document.getElementById('height');
 width.innerHTML = "window inner width is : " + window.innerWidth;
 height.innerHTML = "window inner height is : " + window.innerHeight;
 
-let myWindow;
+var myWindow;
 
 function openWindow(){
     myWindow = window.open("https://www.youtube.com/watch?v=rePN-VFo1Eo&list=PLHiZ4m8vCp9OkrURufHpGUUTBjJhO9Ghy");
@@ -17,3 +22,18 @@ function openWindow(){
 function closeWindow(){
     myWindow.close();
 }
+
+
+// topic 2
+// screen.width /height /availWidth /availHeight /colorDepth /pixelDepth
+
+
+// topic 3
+// window.href /hostname / pathname /protocol
+
+const href = document.getElementById("href");
+const hostname = document.getElementById("hostname");
+const pathname = document.getElementById("pathname");
+const protocol = document.getElementById("protocol");
+
+
