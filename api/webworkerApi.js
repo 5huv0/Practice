14834c53be5427845fe0fe8c9e78a9024b089a97
5huv0 <md88748874@gmail.com>
2024-03-19 00:31:49 +0600

    web worker

--- api/webworkerApi.js
+++ api/webworkerApi.js
@@ -10,6 +10,22 @@ function startWorker(){
             //creating a worker called w
             w = new Worker("worker.js");
         }
+
+        // listening for worker event / message
+
+        w.onmessage = function(event){
+            document.getElementById("demo").innerHTML = event.data;
+        }
+    }
+    else{
+        alert("Your browser does not support web worker!!");
+    }
+}
+
+function stopWorker(){
+    if(typeof Worker !== "undefined"){
+        w.terminate();
+        w = undefined;
     }
     else{
         alert("Your browser does not support web worker!!");
