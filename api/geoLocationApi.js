d21a30a8bf2a80db69f6000f4d6cfc1277d07d01
5huv0 <md88748874@gmail.com>
2024-03-21 01:53:10 +0600

    geolocation done

--- api/geoLocationApi.js
+++ api/geoLocationApi.js
@@ -1,9 +1,35 @@
 const demo = document.getElementById('demo');
 function getLocation(){
     if(navigator.geolocation){
-
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        // navigator.geolocation.watchPosition(showPosition, showError); //this is for continuous position
     }
     else{
-        demo.innerHTML = "Geo Location is not available in your browser!!!"
+        demo.innerHTML("Geo Location is not available in your browser!!!");
     }
+}
+
+function showPosition(position){
+    demo.innerHTML = 
+    "Latitude: " + 
+    position.coords.latitude +
+    "<br/> Longitude: " +
+    position.coords.longitude;
+}
+
+function showError(error){
+   switch (error.code){
+    case error.PERMISSION_DENIED:
+        demo.innerHTML = "Denied Location";
+        break;
+    case error.POSITION_UNAVAILABLE:
+        demo.innerHTML = "unavailable";
+        break;
+    case error.TIMEOUT:
+        demo.innerHTML = "Timeout";
+        break;
+    case error.UNKNOWN_ERROR:
+        demo.innerHTML = "Error";
+        break;
+   }
 }
