b4c63207fb27297fa62c75d4f6e6b2e7c3b161e1
5huv0 <md88748874@gmail.com>
2024-03-21 13:46:58 +0600

    ajax learning

--- Ajax/Lessonn-1.js
+++ Ajax/Lessonn-1.js
@@ -1,5 +1,15 @@
 const demo = document.getElementById("demo");
 
 function loadData(){
-    
+    // creating a new request 
+    const xhr = new XMLHttpRequest();
+
+    // what to do after response arrives
+    xhr.onload = function(){
+        // const container = demo;
+        demo.innerHTML = xhr.responseText;
+    }
+
+    // prepare request methods - get,post,put,options,patch,delete
+    xhr.open("GET" , "./data/data.txt");
 }
