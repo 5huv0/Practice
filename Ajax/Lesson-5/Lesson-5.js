b5906badcf9f3a879bb96e45a92b06fa015dc407
5huv0 <md88748874@gmail.com>
2024-03-26 01:16:04 +0600

    ajax complete but 100% not  undestood

--- Ajax/Lesson-5/Lesson-5.js
+++ Ajax/Lesson-5/Lesson-5.js
@@ -1,18 +1,59 @@
 const get = document.getElementById("get");
 const send = document.getElementById("send");
 
+get.addEventListener("click", getData);
+send.addEventListener("click", sendData);
+
+function sendRequest(method , url , data){
+    const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest(); 
+
+        xhr.onload = function(){
+            if(this.status >= 400){
+                reject(`There was an bug!! and the response status is ${this.status}`);
+            }
+
+            else {
+                resolve(this.response);
+            }
+            
+        }
+
+        xhr.onerror = function(){
+            reject("error");
+        }
+
+
+        xhr.open(method, url);
+
+        xhr.responseType = "json";
+
+        xhr.send(data);
+    })
+
+    return promise;
+}
+
 function getData(){
- const xhr = new XMLHttpRequest();
 
- xhr.onload = function(){
-    console.log(this.responseText);
- }
+    sendRequest("GET", "https://jsonplaceholder.typicode.com/todos/1").then(responseData =>{
+        console.log(responseData);
+    })
 
- xhr.open("GET" , "https://jsonplaceholder.typicode.com/todos/1");
-
- xhr.send();
+    .catch((err) => {
+        console.log(err);
+    });
 }
 
 function sendData(){
 
+    sendRequest("POST",
+     "https://jsonplaceholder.typicode.com/posts", JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      })).then(responseData =>{
+        console.log(responseData);
+    });
 }
+
