67919054660ac13648c329a2763d39214b64dd57
5huv0 <md88748874@gmail.com>
2024-03-26 13:40:08 +0600

    json learning

---
+++ JSON/json.js
@@ -0,0 +1,7 @@
+const json = '{ "name" : "shuvo", "hobby" : "photography", "aim" : "nothing"}';
+
+console.log(json); // This one id json formate
+
+const myObj = JSON.parse(json); // parse is making object or whatever in json formate. json to obj
+
+console.log(myObj); // This is not json this is now a valid object
