4ca2edd3fbcd3295fd72fb9b0395d3eb3ac08fa6
5huv0 <md88748874@gmail.com>
2024-03-30 15:31:42 +0600

    comment on spread operator

--- ES6-prac/spreadOperator.js
+++ ES6-prac/spreadOperator.js
@@ -1,6 +1,6 @@
 var numbers = [1, 2, 3, 4];
 
-var a = [...numbers];
+var a = [...numbers]; // here if i change numbers a will not change it will remain [1,2,3,4]
 
 var b = [...numbers , 6, 7, 8, 9];
 
