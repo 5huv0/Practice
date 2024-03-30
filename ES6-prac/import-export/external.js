e919a9e6af103e6d3f1a7409af2ceccd2dab00b0
5huv0 <md88748874@gmail.com>
2024-03-31 01:05:49 +0600

    es6

---
+++ ES6-prac/import-export/external.js
@@ -0,0 +1,2 @@
+export const a = 8.93;
+export const b = 43.5;
---
+++ ES6-prac/import-export/import.js
@@ -0,0 +1,9 @@
+
+
+import {a , b} from './external.js';
+
+// to import the full file following method
+
+import * as x from './external.js';
+
+console.log(a , b , x);
