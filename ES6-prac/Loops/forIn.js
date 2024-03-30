0867788e48d90a4bbebf8403c7c9d7ceb7f967af
5huv0 <md88748874@gmail.com>
2024-03-30 15:12:15 +0600

    es6 for in for of

---
+++ ES6-prac/Loops/forIn.js
@@ -0,0 +1,11 @@
+// for object iterator
+
+let myObj =  {
+    name : "shuvo",
+    age : 22,
+    hooby : "gaming"
+}
+
+for (x in myObj){
+    console.log(x);
+}
---
+++ ES6-prac/Loops/forOf.js
@@ -0,0 +1,10 @@
+// for array , string iterator
+
+let myArr = [1, 2, 3, 4, 5];
+
+for (x of myArr){
+    console.log(x);
+}
+
+// for of use korle array elements gula pabo 
+// for in use kora jabe in use korle array index pabo
