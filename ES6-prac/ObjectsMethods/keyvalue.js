b1670230014a35227fdd124b647cc939434d5244
5huv0 <md88748874@gmail.com>
2024-03-30 15:30:11 +0600

    es6

---
+++ ES6-prac/ObjectsMethods/keyvalue.js
@@ -0,0 +1,17 @@
+let myObj = {
+    name : "shuvo" ,
+    age : 22,
+    hobby : "gaming"
+};
+
+var key = Object.keys(myObj);
+
+var value = Object.values(myObj);
+
+var entrie = Object.entries(myObj); // entries returns both kay and value
+
+console.log(key);
+
+console.log(value);
+
+console.log(entrie);
---
+++ ES6-prac/spreadOperator.js
@@ -0,0 +1,9 @@
+var numbers = [1, 2, 3, 4];
+
+var a = [...numbers];
+
+var b = [...numbers , 6, 7, 8, 9];
+
+console.log(numbers);
+console.log(a);
+console.log(b);
