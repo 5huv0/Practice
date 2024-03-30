b455708883c5e6ca4bc856c6aa8013f6efb0fb0c
5huv0 <md88748874@gmail.com>
2024-03-31 01:09:39 +0600

    s

--- ES6-prac/import-export/import.js
+++ ES6-prac/import-export/import.js
@@ -1,9 +1,12 @@
 
 
-import {a , b} from './external.js';
+import {a , b, myFunc } from './external.js';
 
 // to import the full file following method
 
 import * as x from './external.js';
 
 console.log(a , b , x);
+
+myFunc();
+
